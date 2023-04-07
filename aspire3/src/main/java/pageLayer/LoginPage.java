cc6a07bd99feb6fb93f2c0d81bd450a707d781fd
pratunwaghmode <112622074+pratunwaghmode@users.noreply.github.com>
2023-04-07 11:46:28 +0530

    Task is completed

---
+++ aspire3/src/main/java/pageLayer/LoginPage.java
@@ -0,0 +1,11 @@
+package pageLayer;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Loginpage is tested");
+
+	}
+
+}
